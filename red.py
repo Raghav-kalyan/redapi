2a391556acae6e073f729176a81ea9fa0479c936
Raghav-kalyan <73018688+Raghav-kalyan@users.noreply.github.com>
2022-12-08 00:52:52 +0530

    Update red.py

--- red.py
+++ red.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify, redirect
 from datetime import datetime 
+
 app = Flask(__name__)
+
 @app.route("/", methods=["GET"])
 def info():
-    return jsonify({'ip': request.remote_addr, 'Host' : request.host, 'Port': request.environ.get( 'REMOTE_PORT' ), 'date': str( datetime.now() ) } )
-@app.get("/status/<url>")
-def redirect_to_url(url):
-    return redirect("http://{url}")
+    return jsonify({'ip': request.remote_addr, 'Host' : request.host, 'Port': request.environ.get('REMOTE_PORT'), 'date':str(datetime.now())})
+
 @app.route("/status/")
-def redirect():
-    return "PLEASE ENTER A VALID URL"    
+def routetourl():
+    return "PLEASE ENTER A VALID URL"
+@app.get("/status/<string:url>")
+def redirect_to_url(url):
+     s = str(url)
+     url = s.replace('-', '.')
+     return redirect(f"http://{url}")
+
 if __name__ == '__main__':
     app.run(debug=True)
