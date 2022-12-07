61bce11ca02096b6d3f319b1cecd0cc2312a6bdb
Raghav-kalyan <73018688+Raghav-kalyan@users.noreply.github.com>
2022-12-07 23:21:12 +0530

    Update red.py
    Dependecies:
    click              8.1.3
    colorama           0.4.6
    Flask              2.2.2
    Flask-Cors         3.0.10
    itsdangerous       2.1.2
    Jinja2             3.1.2
    MarkupSafe         2.1.1
    pip                22.0.4
    python-dateutil    2.8.2
    requests           2.28.1
    setuptools         58.1.0
    urllib3            1.26.13
    virtualenv         20.17.0
    Werkzeug           2.2.2

--- red.py
+++ red.py
@@ -3,9 +3,12 @@ from datetime import datetime
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def info():
-    return jsonify({'ip': request.remote_addr, 'Host' : request.host, 'Port': request.environ.get('REMOTE_PORT'), 'date':str(datetime.now())})
+    return jsonify({'ip': request.remote_addr, 'Host' : request.host, 'Port': request.environ.get( 'REMOTE_PORT' ), 'date': str( datetime.now() ) } )
 @app.get("/status/<url>")
 def redirect_to_url(url):
-    return redirect(f"http://{url}")
+    return redirect("http://{url}")
+@app.route("/status/")
+def redirect():
+    return "PLEASE ENTER VALID URL"    
 if __name__ == '__main__':
     app.run(debug=True)
